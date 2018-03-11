9cf6cfc5afd732c2386c22ccddf4d8e95117542a
N4droJ <36649787+N4droJ@users.noreply.github.com>
2018-03-11 02:43:42 +0000

    Update Documentation.py

--- Documentation.py
+++ Documentation.py
@@ -1,4 +1,6 @@
-// get discord ID from user
-get_discord_id = ctx.message.author.id
-user = await bot.get_user_info(get_discord_id)
-print(get_discord_id)
+# capture discord_id to validate
+@bot.command(pass_context=True)
+async def example(ctx):
+    get_discord_id = ctx.message.author.id
+    user = await bot.get_user_info(get_discord_id)
+    print(get_discord_id)
