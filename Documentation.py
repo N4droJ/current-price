c70b8906c1c5b5154a46841a2c7d135ab9a3a2e5
N4droJ <36649787+N4droJ@users.noreply.github.com>
2018-03-17 01:45:00 +0000

    Update Documentation.py

--- Documentation.py
+++ Documentation.py
@@ -4,3 +4,12 @@ async def example(ctx):
     get_discord_id = ctx.message.author.id
     user = await bot.get_user_info(get_discord_id)
     print(get_discord_id)
+
+# get username and unique ID from discord user that uses the command
+@bot.command(pass_context=True)
+async def getinfo(ctx, vote):
+        getMemberID = ctx.message.author.id
+        getMemberName = ctx.message.author.name
+
+        print (getMemberID)
+        print (getMemberName)
